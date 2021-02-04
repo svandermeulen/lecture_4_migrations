7541018c10e07ea68ed8874f21914407b67bcd91
Meulen <stef.van.der.meulen@ordina.nl>
2021-02-04 14:38:47 +0100

    Configure admin

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -4,3 +4,12 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class AuctionListing(models.Model):
+    user = models.CharField(max_length=255)
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=255)
+    starting_bid = models.FloatField()
+    category = models.CharField(max_length=64)
+    image_url = models.CharField(max_length=255)
